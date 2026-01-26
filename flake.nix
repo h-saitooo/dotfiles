614d7245654565add9ca00d77b06cfec065b903f
Hiroki Saito <36532103+h-saitooo@users.noreply.github.com>
2026-01-26 22:23:02 +0900

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "Personal package management with home-manager";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    homeConfigurations."noppo190" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      modules = [{
+        home.username = "noppo190";
+        home.homeDirectory = "/home/noppo";
+        home.stateVersion = "24.05";
+
+        # インストールしたいパッケージをここに追加
+        home.packages = with nixpkgs.legacyPackages.x86_64-linux; [
+          # 基本的な開発ツール
+          neovim
+          git
+          curl
+          wget
+
+          # シェルツール
+          ghostty
+          fish
+          tmux
+          htop
+
+          # モダンなCLIツール
+          ripgrep  # 高速なgrep代替
+          fd       # 高速なfind代替
+          bat      # catの改良版
+          exa      # lsの改良版
+
+          # その他便利ツール
+          jq       # JSONパーサー
+
+          # ここに必要なパッケージを追加してください
+          # パッケージ検索: https://search.nixos.org/packages
+          # または: nix search nixpkgs パッケージ名
+        ];
+
+        # プログラム固有の設定（オプション）
+        programs.git = {
+          enable = true;
+          userName = "Noppo190";
+          userEmail = "36532103+h-saitooo@users.noreply.github.com";
+        };
+
+        programs.bash = {
+          enable = true;
+          shellAliases = {
+            ll = "ls -la";
+            update = "home-manager switch --flake ~/dotfiles";
+          };
+        };
+
+        # home-managerの自己管理を有効化
+        programs.home-manager.enable = true;
+      }];
+    };
+  };
+}
